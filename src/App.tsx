abafdc60a9b455e0c01f24e12b45a65a736fb5fc
shenkzjay <senksjay@gmail.com>
2025-08-28 20:07:37 +0100

    feat:change ui layout of the client

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Waveform from "./Waveform";
+import { Navbar } from "./components/navbar/navbar";
+import { Cassette } from "./assets/svg/cassette";
 
 // Add type for recordings
 interface Recording {
@@ -13,6 +15,7 @@ function App() {
   const [recordingTime, setRecordingTime] = useState(0);
   const [recordings, setRecordings] = useState<Recording[]>([]);
   const [microphoneError, setMicrophoneError] = useState<string | null>(null);
+  const [cassetteSpin, setCassetteSpin] = useState<boolean>(false);
   const mediaStreamRecorderRef = useRef<MediaRecorder | null>(null);
   const intervalRef = useRef<number | null>(null);
 
@@ -67,6 +70,7 @@ function App() {
       mediaStreamRecorderRef.current = mediaRecorder;
       setIsRecording(true);
       setRecordingTime(0);
+      setCassetteSpin(true);
 
       intervalRef.current = window.setInterval(() => {
         setRecordingTime((prevTime) => prevTime + 1);
@@ -83,6 +87,7 @@ function App() {
     if (mediaStreamRecorderRef.current && mediaStreamRecorderRef.current.state !== "inactive") {
       mediaStreamRecorderRef.current.stop();
       setIsRecording(false);
+      setCassetteSpin(false);
       if (intervalRef.current) {
         clearInterval(intervalRef.current);
         intervalRef.current = null;
@@ -141,101 +146,121 @@ function App() {
 
   return (
     <>
-      <section>
-        <div className="flex flex-col gap-6">
-          <h1>
-            <p className="text-6xl font-bold">Erm..Filler Words?</p>
-          </h1>
-          <div>
-            <p>Hi, welcome to Filler, your personal speech coach</p>
-            <p>We are going to help you become better in your future presentation</p>
-            <p>Click on the button to make a voice record of yourself making a presentation</p>
-          </div>
-          <div className="flex justify-center">
-            <img
-              src="../record.jpg"
-              width={300}
-              height={300}
-              alt="sketch of a cassette player"
-              className="object-contain"
-            />
-          </div>
-        </div>
-
-        <div className="flex justify-center gap-6 mt-6">
-          <div
-            className={`flex flex-row justify-center items-center  ${
-              isRecording ? "bg-blue-100" : "bg-blue-600 text-white"
-            }  w-fit rounded-full p-2 gap-4 border border-blue-200`}
-          >
-            {isRecording && (
-              <div className="flex gap-4 items-center">
-                <Waveform recordingTime={recordingTime} />
-                <span>{formatRecordingTime(recordingTime)}</span>
+      <section className="container mx-auto">
+        <Navbar />
+        <div className="flex gap-40 mx-auto w-full">
+          {/* right section */}
+          <section className="mt-6 w-1/2">
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-4">
+                <p className="text-6xl font-bold">
+                  Lose the <span className="text-title-red">“ums”</span> and{" "}
+                  <span className="text-title-green">Present</span> like a pro!
+                </p>
+                <p>Make smooth presentations without using filler word</p>
+                <p>
+                  Record or upload a presentation and let us analyse and make suggestions on how you
+                  can improve and make presentations like a pro
+                </p>
               </div>
-            )}
-            <button onClick={handleStartRecording} className=" flex ">
-              {isRecording ? (
-                <div className="flex bg-blue-600 p-2 rounded-full gap-2">
-                  <div className="flex item-center justify-center">
-                    <span className="recording-icon bg-white w-4 h-4 rounded-full flex justify-center items-center [transform:translate(0px,2px)] "></span>
+
+              <div className="flex justify-center w-2/3">
+                {/* <img
+                  src="../record.jpg"
+                  width={300}
+                  height={300}
+                  alt="sketch of a cassette player"
+                  className="object-contain legwork"
+                /> */}
+                <Cassette cassetteSpin={cassetteSpin} />
+              </div>
+            </div>
+
+            <div className="flex  gap-6 mt-6">
+              <div
+                className={`flex flex-row   ${
+                  isRecording ? "bg-blue-100" : "bg-blue-600 text-white"
+                }  w-fit rounded-full p-2 gap-4 border border-blue-200`}
+              >
+                {isRecording && (
+                  <div className="flex gap-4 items-center">
+                    <Waveform recordingTime={recordingTime} />
+                    <span>{formatRecordingTime(recordingTime)}</span>
                   </div>
-                  <p className=" w-full flex text-white font-semibold text-sm">Stop Recording</p>
+                )}
+                <button onClick={handleStartRecording} className=" flex ">
+                  {isRecording ? (
+                    <div className="flex bg-blue-600 p-2 rounded-full gap-2">
+                      <div className="flex item-center justify-center">
+                        <span className="recording-icon bg-white w-4 h-4 rounded-full flex justify-center items-center [transform:translate(0px,2px)] "></span>
+                      </div>
+                      <p className=" w-full flex text-white font-semibold text-sm">
+                        Stop Recording
+                      </p>
+                    </div>
+                  ) : (
+                    <p className="p-2 font-semibold "> Click to start recording</p>
+                  )}
+                </button>
+
+                {microphoneError && <p style={{ color: "red" }}>{microphoneError}</p>}
+              </div>
+              <div>
+                <button className="flex flex-row justify-center items-center bg-orange-100 w-fit rounded-full p-4 gap-4 border border-orange-400">
+                  <p className="font-semibold text-orange-500">Upload Audio</p>
+                </button>
+              </div>
+            </div>
+          </section>
+          <section className="w-1/2 ">
+            {/* Recordings List */}
+            <div className="mt-6 bg-slate-100 h-full rounded-2xl p-6">
+              <h3>Your Recordings</h3>
+              {recordings.length === 0 ? (
+                <div className="flex flex-col justify-center items-center h-full gap-6">
+                  <p>No recordings yet!</p>
+                  <p className="w-2/3 text-center">
+                    Click on the “Start recording” button ot upload a pre-recorded presentation to
+                    get started
+                  </p>
                 </div>
               ) : (
-                <p className="p-2 font-semibold "> Start recording</p>
-              )}
-            </button>
-
-            {microphoneError && <p style={{ color: "red" }}>{microphoneError}</p>}
-          </div>
-          <div>
-            <button className="flex flex-row justify-center items-center bg-orange-100 w-fit rounded-full p-4 gap-4 border border-orange-400">
-              <p className="font-semibold text-orange-500">Upload Audio</p>
-            </button>
-          </div>
-        </div>
-
-        {/* Recordings List */}
-        <div style={{ marginTop: "40px" }}>
-          <h3>Your Recordings</h3>
-          {recordings.length === 0 ? (
-            <p>No recordings yet. Start speaking!</p>
-          ) : (
-            <ul style={{ listStyle: "none", padding: 0 }}>
-              {recordings.map((rec, index) => (
-                <li
-                  key={index}
-                  style={{
-                    margin: "10px 0",
-                    padding: "10px",
-                    border: "1px solid #ddd",
-                    borderRadius: "8px",
-                  }}
-                >
-                  <div>
-                    <strong>Recording {recordings.length - index}</strong> -{" "}
-                    {formatTime(rec.timestamp)}
-                  </div>
-                  <audio
-                    src={rec.audioDataUrl}
-                    controls
-                    style={{ marginTop: "10px", width: "100%" }}
-                  />
-                  <div className="flex gap-4 mt-6 text-sm">
-                    <button
-                      onClick={() => handleGetAnalysis(rec)}
-                      className="bg-blue-100 px-4 py-2 rounded-full"
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {recordings.map((rec, index) => (
+                    <li
+                      key={index}
+                      style={{
+                        margin: "10px 0",
+                        padding: "10px",
+                        border: "1px solid #ddd",
+                        borderRadius: "8px",
+                      }}
                     >
-                      <p className="text-blue-600">Get analysis for filler words</p>
-                    </button>
-                    <button>delete</button>
-                    <button>export</button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
+                      <div>
+                        <strong>Recording {recordings.length - index}</strong> -{" "}
+                        {formatTime(rec.timestamp)}
+                      </div>
+                      <audio
+                        src={rec.audioDataUrl}
+                        controls
+                        style={{ marginTop: "10px", width: "100%" }}
+                      />
+                      <div className="flex gap-4 mt-6 text-sm">
+                        <button
+                          onClick={() => handleGetAnalysis(rec)}
+                          className="bg-blue-100 px-4 py-2 rounded-full"
+                        >
+                          <p className="text-blue-600">Get analysis for filler words</p>
+                        </button>
+                        <button>delete</button>
+                        <button>export</button>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </section>
         </div>
       </section>
     </>
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,20 @@
+export function Navbar() {
+  return (
+    <nav>
+      <ul className="flex justify-between">
+        <li>
+          <a>fill_r</a>
+        </li>
+        <li>
+          <a>about</a>
+          <a>uploads</a>
+          <a>pricing</a>
+        </li>
+        <li>
+          <a>Log in</a>
+          <a>sign in</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
